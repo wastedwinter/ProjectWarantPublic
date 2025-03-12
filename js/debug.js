4e80506b5c2b6379ee848be91bf7b6f6430d4d3f
Elliot Davis <recovery12ws@outlook.com>
2025-03-12 08:47:53 -0500

    Update file debug.js

--- js/debug.js
+++ js/debug.js
@@ -1,10 +1,25 @@
 (function () {
-    if (window.location.search.includes("debug")) {
-      var script = document.createElement('script');
-      script.src = "https://cdn.jsdelivr.net/npm/eruda";
-      document.body.appendChild(script);
-      script.onload = function () {
-        eruda.init();
-      };
-    }
-  })();
+  var script = document.createElement('script');
+  script.src = "https://cdn.jsdelivr.net/npm/eruda";
+  document.body.appendChild(script);
+  script.onload = function () {
+      eruda.init();
+  };
+
+  let keysPressed = new Set();
+
+  document.addEventListener('keydown', function (event) {
+      keysPressed.add(event.key);
+      if (keysPressed.has("\\") && keysPressed.has(";")) {  // Backslash + Semicolon
+          if (eruda._isInit) {
+              eruda.toggle();
+          } else {
+              eruda.init();
+          }
+      }
+  });
+
+  document.addEventListener('keyup', function (event) {
+      keysPressed.delete(event.key);
+  });
+})();
